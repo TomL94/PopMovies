7a852c7d11445a7248fc5b4c81668a8ea9ed978c
TomL <lngrtm092@gmail.com>
2015-12-22 00:18:53 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/toml/popmovies/MoviesGrid.java
@@ -0,0 +1,13 @@
+package com.example.toml.popmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesGrid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_grid);
+    }
+}
